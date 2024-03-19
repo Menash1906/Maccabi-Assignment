244d23d8549b6ea17acbd52f02fbdd136b5358ff
Menash1906 <Menashyt@gmail.com>
2024-03-19 13:23:23 +0200

    Initial Commit

---
+++ Maccabi-Assignment/Maccabi_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maccabi_AssignmentApp.swift
+//  Maccabi-Assignment
+//
+//  Created by Michael Menashe on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Maccabi_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
